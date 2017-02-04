bf5c9a507e76c366bbe29a734787f156e036b820
Greg Taylor <hesitatetowonder@gmail.com>
2017-02-04 11:23:13 -0700

    add 5,1 and quater functonality

--- MakeChange.java
+++ MakeChange.java
@@ -1,10 +1,8 @@
 package makeChange;
-import java.util.Scanner;
+
 
 public class MakeChange {
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-		
 		Double itemPrice = Methods.price();
 		
 		Methods.money(itemPrice);
--- Methods.java
+++ Methods.java
@@ -21,7 +21,7 @@ public class Methods {
 		}
 		if (payment < item) {
 			owe = item - payment;
-			System.out.println("Customer still owes " + owe);
+			System.err.println("Error: Customer still owes " + owe);
 			return;
 		}
 		makeChange(item, payment);
@@ -31,34 +31,73 @@ public class Methods {
 	public static void makeChange(double item, double payment) {
 		double change = payment - item, leftOver;
 		String customerChange;
-		if (change > 10) {
+		if (change >= 10) {
 			tenDollar(change);
+		} else if ((change < 10) && (change > 5)) {
+			fiveDollar(change);
+		} else if ((change < 5) && (change > 1)) {
+			oneDollar(change);
+		} else if ((change < 1) && (change > .25)){
+			quarter(change);
 		}
 	}
 
 	public static void tenDollar(double change) {
-		double leftOver = 0;
-		while (change > 10.00) {
-			double tenDollarBill = 1;
+		double leftOver = 0, tenDollarBill = 0;
+		while (change >= 10.00) {
+			tenDollarBill = 1;
 			leftOver = change % 10.0;
 			change = change - 10;
 			tenDollarBill++;
-			System.out.println(tenDollarBill + "Ten Dollar Bill(s)");
 		}
-		if (leftOver > .25){	
+		System.out.println(tenDollarBill + " Ten Dollar Bill(s)");
+		if (leftOver >= 5.00) {
+			fiveDollar(leftOver);
+		} else if (leftOver >= 1.00) {
+			oneDollar(leftOver);
+		} else if (leftOver >= .25) {
 			quarter(leftOver);
 		}
 	}
+
+	public static void fiveDollar(double leftOver) {
+		double fiveDollarBill = 0, oneDollarBill;
+		while (leftOver >= 5.00) {
+			fiveDollarBill = 0;
+			oneDollarBill = leftOver % 5.00;
+			leftOver = leftOver - 5.00;
+			fiveDollarBill++;
+		}
+		System.out.println(fiveDollarBill + " Five Dollar Bill(s)");
+		if ((leftOver >= 1.00) && (leftOver < 5.00)) {
+			oneDollar(leftOver);
+		}
+	}
+
+	public static void oneDollar(double leftOver) {
+		double oneDollarBill = 0, quarter = 1;
+		while (leftOver >= 1.00) {
+			quarter = leftOver % 1;
+			leftOver = leftOver - 1.00;
+			oneDollarBill++;
+		}
+		System.out.println(oneDollarBill + " One Dollar Bill(s)");
+		if ((leftOver >= .25) && (leftOver < 1.00)) {
+			quarter(leftOver);
+		}
+	} 
 	
 	public static void quarter(double leftOver) {
-		double quarter = 0, toDime;
-			while (leftOver > .25) {
-				quarter = 1;
-				toDime = leftOver % .25;
-				leftOver = leftOver - .25;
-				quarter++;
-			}
-		System.out.println(quarter + "Quarter(s)");
+		double quarter = 0;
+		while (leftOver >= .25) {
+			leftOver = leftOver - .25;
+			quarter++;
+		}
+		System.out.println(quarter + " Quarter(s)");
+		System.out.println(leftOver);
+		
 	}
+		
 }
-	
+
+
